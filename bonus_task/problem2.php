e4df4985925c4b11a18a211f24a9e51a51965298
Nahin <nahinulabedin01@gmail.com>
2021-02-22 00:05:22 -0800

    Reg Sub

---
+++ bonus_task/problem2.php
@@ -0,0 +1,22 @@
+
+  <html>
+<head>
+	
+	
+</head>
+  <body>
+  	<?php 
+$colour = array('white', 'green', 'red');
+echo " <i> Output : </i><br> $colour[0], $colour[1], $colour[2], <br>" ;
+ ?> 
+           <ul>
+           	<li>green</li>
+           	<li>red</li>
+           	<li>white</li>
+
+
+           </ul>
+
+  
+  </body>
+  </html>
